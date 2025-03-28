cc620e981ff73e6720a13d4b246832d8519e21cb
Kevin Greer <kevin@fireworks.ai>
2025-03-28 12:26:25 -0400

    fix

--- eks.tf
+++ eks.tf
@@ -65,8 +65,11 @@ resource "aws_launch_template" "launch_template" {
   }
   capacity_reservation_specification {
     capacity_reservation_target {
-      capacity_reservation_resource_group_arn = each.value.capacity_reservation_resource_group_arn
-      capacity_reservation_id                 = each.value.capacity_reservation_id
+      if each.value.capacity_reservation_resource_group_arn != null {
+        capacity_reservation_resource_group_arn = each.value.capacity_reservation_resource_group_arn
+      } else if each.value.capacity_reservation_id != null {
+        capacity_reservation_id = each.value.capacity_reservation_id
+      }
     }
   }
   metadata_options {
